a1b0047cb4162a754ecc6e82ff21341589d2b8b1
frity <nikonovfedor36936@gmail.com>
2020-11-03 01:04:04 +0300

    back: var names refactor, multiple errors

--- back/form.loc/auto.php
+++ back/form.loc/auto.php
@@ -8,14 +8,16 @@ require './db_info/lucifer.php';
 //require './db_info/frity.php';
 $mysql = new mysqli($db_host, $db_user, $db_pass, $db_db);
 
-$res = $mysql->query("SELECT * FROM `vpj_people` WHERE `people_login` = '$login' AND  `people_password` = '$pass'");
-$user = $res->fetch_assoc();
-if(count($user) == 0)
+$account_result = $mysql->query("SELECT * FROM `vpj_people` WHERE `people_login` = '$login' AND  `people_password` = '$pass'");
+$account = $account_result->fetch_assoc();
+$account_exist = count($account) > 0;
+
+if (!$account_exist)
 {
     echo "Неверный логин или пароль";
     exit();
 }
 
-setcookie('user', $user['name'], time() + 3600 * 24 * 30, "/");
+setcookie('account', $account['people_id'], time() + 3600 * 24 * 30, "/");
 $mysql->close();
 ?>
--- back/form.loc/check.php
+++ back/form.loc/check.php
@@ -1,63 +1,79 @@
 <?php
-$name = $_POST['name'];
-$tel = $_POST['tel'];
-$email = $_POST['email'];
-$login = $_POST['login'];
+$name = htmlspecialchars($_POST['name']);
+$tel = htmlspecialchars($_POST['tel']);
+$email = htmlspecialchars($_POST['email']);
+$login = htmlspecialchars($_POST['login']);
 $pass = $_POST['pass'];
 
-$login_len = mb_strlen($login);
-$name_len = mb_strlen($name);
-$email_len = mb_strlen($email);
-$pass_len = mb_strlen($pass);
+function strlen_between($str, $min, $max)
+{
+    $len = mb_strlen($str);
+    return $min <= $len && $len <= $max;
+}
 
-if($login_len < 2 || $login_len > 50)
+function echo_array_json($array)
 {
-    echo "Недопустимая длина логина. От 2 до 50 символов";
-    exit();
-} else if($name_len < 2 || $name_len > 100)
-{
-    echo "Недопустимая длина имени. От 2 до 100 символов";
-    exit();
-} else if($email_len < 2 || $email_len > 60)
-{
-    echo "Недопустимая длина номера e-mail. От 2 до 60 символов";
-    exit();
-} else if($pass_len < 2 || $pass_len > 50)
-{
-    echo "Недопустимая длина пароля. От 2 до 50 символов";
+    echo '["' . implode('","', $array) . '"]';
+}
+
+$errors = [];
+
+if (!strlen_between($login, 2, 50))
+    array_push($errors, "Недопустимая длина логина. От 2 до 50 символов");
+if (!strlen_between($name, 2, 100))
+    array_push($errors, "Недопустимая длина имени. От 2 до 100 символов");
+if (!strlen_between($email, 2, 60))
+    array_push($errors, "Недопустимая длина номера e-mail. От 2 до 60 символов");
+if (!strlen_between($pass, 2, 50))
+    array_push($errors, "Недопустимая длина пароля. От 2 до 50 символов");
+
+if (count($errors) > 0) {
+    echo_array_json($errors);
     exit();
 }
 
 $pass = md5($pass."anenght2ei4");
 
-require './db_info/lucifer.php';
-//require './db_info/frity.php';
+//require './db_info/lucifer.php';
+require './db_info/frity.php';
 $mysql = new mysqli($db_host, $db_user, $db_pass, $db_db);
 
-$log = $mysql->query("SELECT * FROM `vpj_people` WHERE `people_login` = '$login'");
-$telephone = $mysql->query("SELECT * FROM `vpj_people` WHERE  `people_phone` = '$tel'");
-$em = $mysql->query("SELECT * FROM `vpj_people` WHERE `people_email` = '$email'");
-$use1 = $log->fetch_assoc();
-$use2 = $telephone->fetch_assoc();
-$use3 = $em->fetch_assoc();
+$login_result = $mysql->query("SELECT * FROM `vpj_people` WHERE `people_login` = '$login'");
+$phone_result = $mysql->query("SELECT * FROM `vpj_people` WHERE  `people_phone` = '$tel'");
+$email_result = $mysql->query("SELECT * FROM `vpj_people` WHERE `people_email` = '$email'");
 
-if(count($use1) > 0)
-{
-    echo "Такой логин уже есть";
-    exit();
-}
-if(count($use2) > 0)
-{
-    echo "Такой номер телефона уже используется";
-    exit();
-}
-if(count($use3) > 0)
-{
-    echo "Такой email уже используется";
+$login_exist = $login_result && $login_result->num_rows > 0;
+$phone_exist = $phone_result && $phone_result->num_rows > 0;
+$email_exist = $email_result && $email_result->num_rows > 0;
+
+$errors = [];
+if ($login_exist)
+    array_push($errors, "Такой логин уже есть");
+if ($phone_exist)
+    array_push($errors, "Такой номер телефона уже используется");
+if ($email_exist)
+    array_push($errors, "Такой email уже используется");
+
+if (count($errors) > 0) {
+    echo_array_json($errors);
     exit();
 }
 
-$mysql->query("INSERT INTO `vpj_people` (`people_name`, `people_login`, `people_password`, `people_phone`, `people_email`) VALUES('$name','$login','$pass', '$tel', '$email')");
+$mysql->query(
+    "INSERT INTO `vpj_people` (
+        `people_name`, 
+        `people_login`, 
+        `people_password`, 
+        `people_phone`, 
+        `people_email`
+    ) VALUES (
+        '$name',
+        '$login',
+        '$pass',
+        '$tel',
+        '$email'
+    )"
+);
 
 $mysql->close();
 header('Location: ./');
